f49f6079d1450b2ae3ceae06eb74344e2945748b
xinliu-c <2323953781@qq.com>
2020-07-30 13:50:23 +0800

    Initial Commit

---
+++ prac2/randoms.py
@@ -0,0 +1,11 @@
+import random
+print(random.randint(5,20))
+#6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+# the smallest number that I have seen from line 1 is 6 and the largest number is 20.
+print(random.randrange(3,10,2))
+#3 5 7 9
+# the smallest number is 3 and the largest number is 9. 4 is not produced.
+print(random.uniform(2.5,5.5))
+#2.5680584612595387 2.6238724682531056 3.1920848182450072 4.5983999559095174 4.630839335176211 5.022611418655931 ...
+# the smallest number is 2.5680584612595387 and the largest number is 5.244902518628277.
+print(random.randrange(1,101))
