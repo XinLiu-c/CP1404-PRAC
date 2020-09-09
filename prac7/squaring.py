17347be12efbc450a6e674d89689811091b8d955
xinliu-c <2323953781@qq.com>
2020-09-09 22:55:23 +0800

    use kivy

---
+++ prac7/squaring.py
@@ -0,0 +1,24 @@
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class SquareNumberApp(App):
+    """ SquareNumberApp is a Kivy App for squaring a number """
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.size = (200, 100)
+        self.title = "Square"
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        try:
+            result = int(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text=""
+
+
+SquareNumberApp().run()
